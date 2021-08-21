e7e87fb1f0fcca8eb61a5229c97444a815c703b2
Jaap17 <jaapanjaria17@gmail.com>
2021-08-21 12:06:37 +0530

    commit

--- LAB4/Wine_dataset_DT.py
+++ LAB4/Wine_dataset_DT.py
@@ -11,7 +11,7 @@ y = dataset.target
 
 
 #splitting test and training data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
 
 # Intializing the decsion tree Classifier
 model = tree.DecisionTreeClassifier()
